65629fa0f53ed03b9eb7fc518c42452dcfed01aa
Cindy Marin Torres <cindymarintorres@hotmail.com>
2026-04-13 14:13:01 -0500

    feat: add puerto en nest para aceptar conexiones externas

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,14 +6,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
   app.setGlobalPrefix('api');
-  
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
     transform: true,
   }));
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0')  // ← acepta conexiones desde fuera
   console.log(`Application is running on: http://localhost:${port}/api`);
 }
 bootstrap();
